37277e8b30b1ce4d03e10fff43afc46e548984fd
AdManteza <vagrant@lighthouse-labs>
2016-08-08 17:08:20 +0000

    completed steps 1 to 7

---
+++ app/actions.rb
@@ -0,0 +1,29 @@
+# Homepage (Root path)
+get '/' do
+  erb :index
+end
+
+get '/tracks' do
+  @tracks = Track.all
+  erb :'tracks/index'
+end
+
+get '/tracks/new' do
+  erb :'tracks/new'
+end
+
+get '/tracks/:id' do
+  @track = Track.find params[:id]
+  erb :'tracks/show'
+end
+
+post '/tracks' do
+  @track = Track.new(
+    song_title: params[:song_title],
+    artist: params[:artist],
+    url:  params[:url]
+  )
+  @track.save
+  redirect '/tracks'
+end
+
